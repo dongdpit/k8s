6158862b4fd38caa29892101c4483d5150a5932d
dongdpit <165973148+dongdpit@users.noreply.github.com>
2024-08-23 14:43:35 +0700

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -24,16 +24,6 @@ echo
 echo "**** Update repository package ****"
 echo 
 
-export http_proxy=http://10.26.2.55:8080/
-export https_proxy=http://10.26.2.55:8080/
-
-cat <<EOF | sudo tee /etc/environment
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
-export http_proxy="http://10.26.2.55:8080"
-export https_proxy="http://10.26.2.55:8080"
-export no_proxy="localhost,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,169.254.0.0/16,127.0.0.0/8"
-EOF
-
 apt update
 apt install -y ca-certificates curl apt-transport-https software-properties-common
 
@@ -74,8 +64,8 @@ echo
 mkdir /etc/systemd/system/crio.service.d
 cat <<EOF | sudo tee /etc/systemd/system/crio.service.d/http-proxy.conf
 [Service]
- Environment="HTTP_PROXY=10.26.2.55:8080"
- Environment="HTTPS_PROXY=10.26.2.55:8080"
+ Environment="HTTP_PROXY="
+ Environment="HTTPS_PROXY="
  Environment="NO_PROXY=10.0.0.0/8,169.254.0.0/16,172.16.0.0/12,192.168.0.0/16,127.0.0.0/8,localhost,127.0.0.1"
 EOF
 
